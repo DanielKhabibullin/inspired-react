1d3b5fdc8e8ac1ce43a17f1db7ddb32c6ab0380b
Daniel Khabibullin <i6unny@gmail.com>
2023-07-10 12:10:24 +0500

    Add Header

---
+++ src/Components/Layout/Container/Container.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+import cn from 'classnames';
+import style from './Container.module.scss';
+
+export const Container = ({className, children}) =>
+	<div className={cn(style.container, className)}>{children}</div>;
